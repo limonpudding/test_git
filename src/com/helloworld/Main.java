6c342926725a0b77d1cdd89565b60d555c5af59c
limonpudding <martyshenko.sergei@yandex.ru>
2018-07-30 16:11:20 +0500

    test merge

--- src/com/helloworld/Main.java
+++ src/com/helloworld/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         // write your code here
-        System.out.println("Hello world!");
+        System.out.println("Hello world! test merge");
         System.out.println("master branch");
     }
 }
