9f8f834cee8b933ebe12447bbab3ae7d5bc71c8e
limonpudding <martyshenko.sergei@yandex.ru>
2018-07-30 16:41:26 +0500

    new content in master branch

--- src/com/helloworld/Main.java
+++ src/com/helloworld/Main.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         // write your code here
-        System.out.println("Hello world! test merge");
+        System.out.println("Hello world! test merge" + "THIS IS A NEW CONTENT IN MATER BRANCH");
         System.out.println("master branch");
     }
 }
