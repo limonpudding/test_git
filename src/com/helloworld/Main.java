afab5200e6318edf6439e4a8ac3b2d774287eae6
limonpudding <martyshenko.sergei@yandex.ru>
2018-07-30 16:39:37 +0500

    newfoo

--- src/com/helloworld/Main.java
+++ src/com/helloworld/Main.java
@@ -6,5 +6,10 @@ public class Main {
         // write your code here
         System.out.println("Hello world! test merge");
         System.out.println("master branch");
+        System.out.println(sum(1,2,3));
+    }
+
+    public static int sum(int a, int b, int c) {
+        return a + b + c;
     }
 }
