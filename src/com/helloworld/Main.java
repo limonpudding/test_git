8fe78c001983cdce37317e33b798a72b4c4c67ee
limonpudding <martyshenko.sergei@yandex.ru>
2018-07-30 17:00:20 +0500

    func sub

--- src/com/helloworld/Main.java
+++ src/com/helloworld/Main.java
@@ -7,9 +7,14 @@ public class Main {
         System.out.println("Hello world! test merge");
         System.out.println("master branch");
         System.out.println(sum(1,2,3));
+        System.out.println(sub(10, 4));
     }
 
     public static int sum(int a, int b, int c) {
         return a + b + c;
+    }
+
+    public static int sub(int a, int b) {
+        return a - b;
     }
 }
